7de8a0a495f2aae429d872609f228a1ff2db5328
ezequiel <pereiraezequiel330@gmail.com>
2021-10-16 13:12:49 -0300

    a

--- loja virtual/script.js
+++ loja virtual/script.js
@@ -2,19 +2,19 @@ const items =[
     {
         id: 0,
         nome:'camiseta',
-        img:'imagens/foto.png',
+        img:'imagens/foto.jpg',
         quantidade: 0
     },
     {
         id: 1,
         nome:'short',
-        img:'imagens/foto.png',
+        img:'imagens/short.jpg',
         quantidade: 0
     },
     {
         id: 2,
         nome:'sapato',
-        img:'imagens/foto.png',
+        img:'imagens/sapato.jpg',
         quantidade: 0
     },
 ]
@@ -29,7 +29,7 @@ inicializarloja = () => {
         <div class="produto-single">
             <img src="`+vaL.img+`" />
             <p>`+vaL.nome+` </p>
-            <a key="`+vaL.id+`" href="">Adicionar ao carrinha!</a>
+            <a key="`+vaL.id+`" href="#">Adicionar ao carrinha!</a>
             </div>
 
         
@@ -42,10 +42,41 @@ inicializarloja = () => {
 
 inicializarloja();
 
+atualizarcarrinho =() => {
+    var containercarrinho = document.getElementById('carrinho')
+    containercarrinho.innerHTML =""
+    items.map((vaL)=>{
+        if(vaL.quantidade >0){
+        containercarrinho.innerHTML+=`
+        <div class="info-single-sheckout">
+
+            <p style="float:left;">Produto: `+vaL.nome+`</p>
+            <p style="float:right;">Quantidade: `+vaL.quantidade+`</p>
+            <div style="clear:both"></div>
+        </div>
+        `;
+
+        }
+    })
+
+}
+
+var links = document.getElementsByTagName('a');
+
+for(var i =0; i <links.length; i++){
+    links[i].addEventListener("click",function(){
+        let key =this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarcarrinho();
+        return false;
+
+
+    })
 
 
 
-
+}
+    
 
 
 
